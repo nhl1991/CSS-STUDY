33d8ccf427c144e6fb40df854c9fbaf4b3c9511e
nhl1991 <laslark1991@gmail.com>
2025-01-20 16:52:53 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: 'export',
+  basePath: '/project-0120',
 };
 
 export default nextConfig;
