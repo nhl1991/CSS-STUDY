76d48365fb7462e8145f99f0aaccda29b26fbe3f
Laslark <laslark1991@gmail.com>
2025-08-29 23:46:54 +0900

    Fix UI app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,33 @@
 import Link from "next/link";
 
 export default function Page() {
+  return (
+    <div className="w-screen h-screen p-12 flex items-center justify-center">
+      <div>
+        <ul className="flex flex-row">
+          <li>
+            <Link className="" href={"/boxsizing"}>
+              BOX-SIZING
+            </Link>
+          </li>
+          <li>
+            <Link className="" href={"/viewport-unit"}>
+              Viewport Unit
+            </Link>
+          </li>
 
-
-
-    return (
-        <div className="w-screen h-screen p-12 flex items-center justify-center">
-            <ul className="flex flex-row">
-                <li>
-                    <Link className="" href={'/boxsizing'}>BOX-SIZING</Link>
-                </li>
-                <li>
-                    <Link className="" href={'/viewport-unit'}>Viewport Unit</Link>
-                </li>
-
-                <li>
-                    <Link className="" href={'/grid'}>GRID-LAYOUT</Link>
-                </li>
-                <li>
-                    <Link className="" href={'/clamp'}>clamp</Link>
-                </li>
-            </ul>
-        </div>
-    )
+          <li>
+            <Link className="" href={"/grid"}>
+              GRID-LAYOUT
+            </Link>
+          </li>
+          <li>
+            <Link className="" href={"/clamp"}>
+              clamp
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
